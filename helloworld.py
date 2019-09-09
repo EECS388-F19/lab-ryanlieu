3a76e71940fb9c521b223ca9b4d24b724cef6eed
ryanjlieu <ryanlieu@umich.edu>
2019-09-09 15:18:33 -0400

    helloworld file

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+name = 'Ryan'
+rand_one = random.randint(0,100)
+rand_two = random.randint(0,100)
+rand_sum = rand_one + rand_two
+print(name)
+print('{}{}'.format(rand_one, rand_two))
+print('Sum = {}'.format(rand_sum))
+print('Average = {}'.format(rand_sum / 2.0))
